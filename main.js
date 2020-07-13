4eb2909b1226e35b931884d6a29b00f4fdd329dd
vagorobets <44757226+vagorobets@users.noreply.github.com>
2020-07-13 18:16:34 +0300

    add bin function

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 /*  Функционал:
     1. Перевод из одной системы в другую при вводе значения;
-    2. При потере фокуса стираются введенные значения;
+    2. Не используя parseInt();
     3. Во второй инпут можно вводить только цифры 1 и 0;
     4. Кнопки изменения числа в инпуте отсутствуют;
+    5. В первый инпут можно вводить только положительные числа;
 */
 
 let dec = document.querySelector('#dec');
@@ -21,4 +22,12 @@ dec.addEventListener('input', function(){
     }
 
     bin.value = arr.reverse().join('');
+})
+
+bin.addEventListener('input', function(){
+    let result = 0;
+    bin.value.split('').reverse().forEach((elem, index) => 
+        result +=(+elem * Math.pow(2, index))
+        );
+    dec.value = result;
 })
