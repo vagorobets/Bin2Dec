0c1d0643fc730ece2f7688ecd277c3b451983665
vagorobets <44757226+vagorobets@users.noreply.github.com>
2020-07-13 16:33:22 +0300

    add some function

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
-let input = document.querySelector('#input');
-let result = document.querySelector('#result');
+/*  Функционал:
+    1. Перевод из одной системы в другую при вводе значения;
+    2. При потере фокуса стираются введенные значения;
+    3. Во второй инпут можно вводить только цифры 1 и 0;
+    4. Кнопки изменения числа в инпуте отсутствуют;
+*/
+
+let dec = document.querySelector('#dec');
+let bin = document.querySelector('#bin');
+let test = document.querySelector('#test');
+
+dec.addEventListener('input', function(){
+    let num = +dec.value;
+    let arr = [];
+    while(num > 1){
+        if(Math.floor(num) % 2 == 0) arr.push(0);
+        else {            
+            arr.push(1);
+        }
+        num = num / 2;
+    }
+
+    bin.value = arr.reverse().join('');
+})
